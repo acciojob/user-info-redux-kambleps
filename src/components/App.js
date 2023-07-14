2ab0eb1987b7d8306ba050906d6cd9346380b40b
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-14 14:41:08 +0000

    second

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,22 @@ import {updateName,updateEmail} from "./action/actionCreater";
 
 const App = () => {
   const name = useSelector(state=>state.name);
-  const email = useSelector(state=>state.email);
-  const dispatch = useDispatch();
+    const email = useSelector(state=>state.email);
+    const dispatch = useDispatch();
+  
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>User Information</h1>
-        <label for="name">Name:</label>
-        <input type="text" id="name" onChange={(e)=>dispatch(updateName(e.target.value))} /></br>
-        <label for="email">Email:</label>
-        <input type="text" id="email" onChange={(e)=>dispatch(updateEmail(e.target.value))} />
-        <p> Current value of Store:</p>
-        <p> Name-{name}</p>
-        <p> Email-{email}</p>
+      <h1>User Information</h1>
+      <label for="name">Name:</label>
+      <input type="text" id="name" onChange={(e)=>dispatch(updateName(e.target.value))}></input><br></br>
+      <label for="email">Email:</label>
+      <input type="text" id="email" onChange={(e)=>dispatch(updateEmail(e.target.value))}></input>
+      <h3>Current values in Store:</h3>
+      <p>Name:{name}</p>
+      <p>Email:{email}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
